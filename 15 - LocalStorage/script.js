ea2795e4287a3092b9e67472f05b7bb465ae4b0c
hellogaray <137473678+hellogaray@users.noreply.github.com>
2024-12-26 12:00:00 +0900

    Updating files

--- 15 - LocalStorage/script.js
+++ 15 - LocalStorage/script.js
@@ -1,3 +1,22 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
+
+function addItem(event) {
+    event.preventDefault();
+    const text = (this.querySelector('[name=item')).value;
+    const item = {
+        text,
+        done: false
+    }
+    items.push(item)
+
+    this.reset();
+
+}
+
+
+function populateList(items = [], platesList) {
+
+}
+addItems.addEventListener('submit', addItem);
